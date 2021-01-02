e4c9f38c956d4984ed08ef0dadf2bb748715e8cc
Mark Koszykowski <31666939+markkoszykowski@users.noreply.github.com>
2021-01-02 16:44:01 -0500

    Add files via upload

---
+++ Problem Set 7/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+// Tamar Bacalu & Mark Koszykowski
+// ECE357 - Operating Systems Problem Set 7, 2A
+
+int main() {
+	printf("hello world\n");
+	_exit(0);
+}
